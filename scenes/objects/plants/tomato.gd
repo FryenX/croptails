da50d90b5f6610479777949d4e46b3a835f79a8e
FryenX <ardiwidana@gmail.com>
2025-07-06 22:49:18 +0800

    adding crops growth using day and night cycles

---
+++ scenes/objects/plants/tomato.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+var tomato_harvest_scene = preload("res://scenes/objects/plants/harvest_tomato.tscn")
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var watering_particle: GPUParticles2D = $WateringParticle
+@onready var flowering_particle: GPUParticles2D = $FloweringParticle
+@onready var growth_cycle_component: GrowthCycleComponent = $GrowthCycleComponent
+@onready var hurt_component: HurtComponent = $HurtComponent
+
+var growth_state: DataTypes.GrowthStates = DataTypes.GrowthStates.Seed
+var start_tomato_frame_offset: int = 6
+
+func _ready() -> void:
+	watering_particle.emitting = false
+	flowering_particle.emitting = false
+	
+	hurt_component.hurt.connect(on_hurt)
+	growth_cycle_component.crop_maturity.connect(on_crop_maturity)
+	growth_cycle_component.crop_harvesting.connect(on_crop_harvesting)
+	
+func _process(delta: float) -> void:
+	growth_state = growth_cycle_component.get_current_growth_state()
+	sprite_2d.frame = growth_state + start_tomato_frame_offset
+	
+	if growth_state == DataTypes.GrowthStates.Maturity:
+		flowering_particle.emitting = true
+	
+func on_hurt(hit_damage: int) -> void:
+	if !growth_cycle_component.is_watered:
+		watering_particle.emitting = true
+		await get_tree().create_timer(5.0).timeout
+		watering_particle.emitting = false
+		growth_cycle_component.is_watered = true
+		
+func on_crop_maturity() -> void:
+	flowering_particle.emitting = true
+	
+func on_crop_harvesting() -> void:
+	var tomato_harvest_scene = tomato_harvest_scene.instantiate() as Node2D
+	tomato_harvest_scene.global_position = global_position
+	get_parent().add_child(tomato_harvest_scene)
+	queue_free()
