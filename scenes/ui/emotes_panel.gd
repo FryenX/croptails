027e4ae85694f97ff570b378271a737593bae3f5
FryenX <ardiwidana@gmail.com>
2025-07-10 17:12:47 +0800

    Adding Chests for Reward

--- scenes/ui/emotes_panel.gd
+++ scenes/ui/emotes_panel.gd
@@ -9,6 +9,9 @@ func _ready() -> void:
 	randomize()
 	animated_sprite_2d.play("emote_1_idle")
 	emote_idle_timer.timeout.connect(_on_emote_idle_timer_timeout)
+	
+	InventoryManager.inventory_changed.connect(on_inventory_changed)
+	GameDialogueManager.feed_the_animals.connect(on_feed_the_animals)
 
 func play_emote(animation: String) -> void:
 	animated_sprite_2d.play(animation)
@@ -17,3 +20,9 @@ func _on_emote_idle_timer_timeout() -> void:
 	var index = randi_range(0, idle_emotes.size() - 1)
 	var emote = idle_emotes[index]
 	animated_sprite_2d.play(emote)
+
+func on_inventory_changed() -> void:
+	animated_sprite_2d.play("emote_7_excited")
+
+func on_feed_the_animals() -> void:
+	animated_sprite_2d.play("emote_6_love_kiss")
